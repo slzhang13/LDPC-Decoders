df3bbc3bd3f613b4e9e2c3da08ca660c7dd80c62
slzhang13 <slzhang13@qq.com>
2022-10-01 23:13:07 +0800

    m file for NMSA completed and verified

--- NMSA_Layered_Decoding_m.m
+++
@@ -1,5 +0,0 @@
-function [vn_llr_app, cn_llr_ext, termi_stat] = NMSA_Layered_Decoding_m(H_dec, vn_llr_app, cn_llr_ext, iter_max, termi_method)
-
-    % Reference: MATLAB document of the built-in function ldpcDecode (R2021b+)
-
-end
